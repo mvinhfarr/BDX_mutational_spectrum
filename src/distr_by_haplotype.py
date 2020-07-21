971dd7b86f706a88e3167cf8b953c8af75e371ff
Myles Vinh Farr <mylesvf37@gmail.com>
2020-07-20 19:13:05 -0700

    Initial Visualizations of Mutation Spectrum

--- src/distr_by_haplotype.py
+++ src/distr_by_haplotype.py
@@ -9,6 +9,8 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
+import seaborn as sb
+
 
 def load_data(data_dir):
     chr_singletons = []
@@ -24,7 +26,6 @@ def load_data(data_dir):
 
 def filter_raw_data(raw_summary, remove_hetero=True):
     df = pd.DataFrame(raw_summary.loc[:, ['chrom', 'bxd_strain', 'haplotype', 'kmer', 'gt']])
-    # df = raw_summary.copy(deep=True)
 
     # remove indel mutations
     df = df[~df['kmer'].str.contains('indel')]
@@ -43,7 +44,7 @@ def get_complement(seq):
     return seq.translate(base_map)
 
 
-def classify_snv(kmer, include_flank_nuc=True):
+def classify_kmer(kmer, include_flank_nuc=True):
     snv = [kmer[1], kmer[-2]]
 
     '''
@@ -61,19 +62,25 @@ def classify_snv(kmer, include_flank_nuc=True):
 
 
 def convert_kmers(df):
-    kmer_df = pd.DataFrame(df['kmer'].apply(lambda k: classify_snv(k, include_flank_nuc=True)).values.tolist(), index=df.index)
+    kmer_df = pd.DataFrame(df['kmer'].apply(lambda k: classify_kmer(k, include_flank_nuc=True)).values.tolist(), index=df.index)
     df = df.drop('kmer', axis=1)
     converted = pd.concat([df, kmer_df], axis=1)
     return converted
 
 
-def visualize(df):
+def visualize_mutation_spectrum(df):
     snv_list = ['A>C', 'A>G', 'A>T', 'C>A', 'C>G', 'C>T']
 
     # distribution of snv's
     snv_counts = df['snv'].value_counts().reindex(index=snv_list)
+
+    fig1= plt.figure(1)
+
     plt.bar(snv_list, snv_counts)
-    plt.savefig('out/snv_distr.pdf')
+
+    plt.ylabel('Counts')
+    plt.xlabel('Mutation Type')
+    plt.title('Mutation Spectrum Counts')
 
     # distribution by haplotype
     bl6j_df = df[df['haplotype'] == 0]
@@ -85,16 +92,73 @@ def visualize(df):
     x = np.arange(len(snv_list))
     width = 0.35
 
-    fig, ax = plt.subplots()
-    bl6j_bar = ax.bar(x - width/2, bl6j_snv_counts, width=width, label='C57BL/6J')
-    dba2j_bar = ax.bar(x + width / 2, dba2j_snv_counts, width=width, label='DBA/2J')
+    fig2, ax2 = plt.subplots()
+    ax2.bar(x, bl6j_snv_counts, width=width, align='edge', label='C57BL/6J')
+    ax2.bar(x, dba2j_snv_counts, width=-width, align='edge', label='DBA/2J')
 
-    ax.set_ylabel('Counts')
-    ax.set_title('Mutation Spectrum Counts by Haplotype')
-    ax.set_xticklabels(snv_list)
-    ax.legend()
+    ax2.set_ylabel('Counts')
+    ax2.set_title('Mutation Spectrum Counts by Haplotype')
+    ax2.set_xticklabels(snv_list)
+    ax2.set_xlabel('Mutation Type')
+    ax2.legend()
 
-    fig.tight_layout()
-    # fig.savefig('out/snv_distr_by_haplo_new.pdf')
+    fig1.tight_layout()
+    fig2.tight_layout()
+    fig1.savefig('out/snv_distr.pdf')
+    fig2.savefig('out/snv_distr_by_haplo_new.pdf')
 
     plt.show()
+
+
+def visualize_strain_distrb(df):
+    strain_counts = df['bxd_strain'].value_counts()
+    fig = plt.figure(1)
+    plt.bar(strain_counts.index, strain_counts)
+    plt.xticks([])
+    plt.xlabel('Strains')
+    plt.ylabel('# Mutations')
+    plt.title('Number of Mutations per Strains')
+
+    plt.tight_layout()
+    # plt.savefig('out/numSNV_per_strain')
+
+    plt.show()
+    print(strain_counts)
+
+
+def convert_into_proportion(df):
+    multi_index_names = [['C>A', 'C>G', 'C>T', 'A>G', 'A>C', 'A>T'], ['A', 'T', 'C', 'G']]
+    propsdf = pd.DataFrame(index=pd.MultiIndex.from_product(multi_index_names, names=('SNV', '5\'')), columns=['A', 'T', 'C', 'G'], dtype=float)
+
+    snv_counts = df['kmer'].value_counts()
+    tot = snv_counts.sum()
+    snv_props = snv_counts/tot
+
+    for index, row in snv_props.iteritems():
+        kmer = classify_kmer(index)
+        propsdf.loc[(kmer['snv'], kmer['5-base']), kmer['3-base']] = row
+
+    return propsdf
+
+
+def mutation_spectrum_heatmap(df):
+    b_df = df[df['haplotype'] == 0]
+    d_df = df[df['haplotype'] == 1]
+
+    b_props = convert_into_proportion(b_df)
+    d_props = convert_into_proportion(d_df)
+
+    ratio_props = b_props/d_props
+
+    print(ratio_props)
+
+    # tempdf = np.random.random((16,16))
+
+    ax = sb.heatmap(ratio_props, cmap='bwr', cbar=True, square=True)
+    plt.title('Ratio of proportions of SNVs between BL6: DBA')
+    plt.tight_layout()
+
+    plt.savefig('out/ratio_props_heatmap')
+    plt.show()
+
+
--- src/main.py
+++ src/main.py
@@ -7,12 +7,16 @@ data_dir = 'data/per_chr_singleton'
 results_dir = 'out/'
 
 raw_singleton_summary = distr_by_haplotype.load_data(data_dir)
-raw_singleton_summary.to_csv(results_dir + 'raw_singleton_summary')
+# raw_singleton_summary.to_csv(results_dir + 'raw_singleton_summary')
 
 filtered_singletons = distr_by_haplotype.filter_raw_data(raw_singleton_summary)
-filtered_singletons.to_csv(results_dir + 'filtered_singletons')
+# filtered_singletons.to_csv(results_dir + 'filtered_singletons')
 
 formatted_singletons = distr_by_haplotype.convert_kmers(filtered_singletons)
-formatted_singletons.to_csv(results_dir + 'formatted_singletons')
+# formatted_singletons.to_csv(results_dir + 'formatted_singletons')
 
-distr_by_haplotype.visualize(formatted_singletons)
+# distr_by_haplotype.visualize(formatted_singletons)
+
+# distr_by_haplotype.visualize_strain_distrb(formatted_singletons)
+
+distr_by_haplotype.mutation_spectrum_heatmap(filtered_singletons)
