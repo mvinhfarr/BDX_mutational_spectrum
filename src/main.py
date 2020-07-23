c759f855e972ff76f313724bfdd0e63ccc4c409e
Myles Vinh Farr <mylesvf37@gmail.com>
2020-07-22 18:57:13 -0700

    Revisualized distributions

--- src/main.py
+++ src/main.py
@@ -17,14 +17,70 @@ results_figs = 'out/figs/'
 raw_df = pd.read_csv(results_df+'raw_singleton_summary', index_col=0)
 filtered_df = pd.read_csv(results_df+'filtered_singletons', index_col=0)
 formatted_df = pd.read_csv(results_df+'formatted_singletons', index_col=0)
-muts_strains = pd.read_csv(results_df+'mutation_strains', index_col=[0, 1, 2, 3])
+mutation_strain_df = pd.read_csv(results_df+'mutation_strains', index_col=[0, 1, 2, 3])
 
-visualize.mutation_spectrum_barcharts(muts_strains, show=True, save=True, results_dir=results_figs)
+# snv_tots = mutations_strains.sum(axis=0, level=[0, 1, 2]).sum(axis=1)
 
-# preprocess.visualize(formatted_singletons)
+# snv_tots.unstack([0]).plot(kind='bar')
+# plt.bar(snv_tots.index)
+# plt.show()
 
-# preprocess.visualize_strain_distrb(formatted_singletons)
+# snv_df, bl_df, dba_df = visualize.mutation_spectrum_barcharts(mutations_strains, show=True, save=False, results_dir=results_figs)
 
-# ratio_props = preprocess.mutation_spectrum_heatmap(filtered_singletons)
-# ratio_props.to_csv(results_dir + 'ratio_props')
+# bl_dba_ratios = bl_df['fracs'] / dba_df['fracs']
 
+# the number of mutations per strain
+mutations_per_strain = mutation_strain_df.sum(axis=0)
+# mutation spectrum as a fraction per strain
+# i.e. num of a mutation type per strain divided by tot num mutations per strain
+mutation_fraction_df = mutation_strain_df / mutations_per_strain
+# find the average mutation spectrum and them sum rows to collapse into 6 snv types
+# e.g. C>T = 0.472115 means on average C>T comprised 47.5% of the mutations in a strain
+snv_strain_avg_frac = mutation_fraction_df.mean(axis=1).sum(axis=0, level=0)
+
+# sum of mutation spectrum --> collapse into 6 snvs and sum for all strains
+snv_tots = mutation_strain_df.sum(axis=0, level=0).sum(axis=1)
+# total total number of mutations
+tot = snv_tots.sum()
+# mutation spectrum as fraction of total mutations
+# e.g. C>T = 0.470441 means 47% of the total mutations recorded were C to T
+snv_frac = snv_tots / tot
+
+haplo_snv_avg = mutation_fraction_df.mean(axis=1).sum(axis=0, level=[0, 3])
+
+bl_snv_tots = mutation_strain_df.xs('BL', level='ht').sum(axis=0, level=0).sum(axis=1)
+bl_tot = bl_snv_tots.sum()
+bl_snv_frac = bl_snv_tots / bl_tot
+dba_snv_tots = mutation_strain_df.xs('DBA', level='ht').sum(axis=0, level=0).sum(axis=1)
+dba_tot = dba_snv_tots.sum()
+dba_snv_frac = dba_snv_tots / dba_tot
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
+
+# snv_frac.plot(kind='bar', ax=ax1)
+snv_strain_avg_frac.plot(kind='bar', ax=ax1)
+
+haplo_snv_avg.unstack(1).plot(kind='bar', ax=ax4)
+ax2.bar(bl_snv_frac.index, bl_snv_frac, align='edge', width=0.35, label='C57BL/6J')
+ax2.bar(dba_snv_frac.index, dba_snv_frac, align='edge', width=-0.35, label='DBA/2J')
+
+temp = mutation_strain_df.sum(axis=1)
+temp.unstack([1]).plot(kind='bar', ax=ax3, stacked=True)
+
+print(mutation_fraction_df)
+print(haplo_snv_avg)
+
+fig.tight_layout()
+plt.show()
+
+'''
+snv_df = mutations_strains.sum(axis=1)
+snv_df = snv_df / snv_df.sum()
+
+bl_df = mutations_strains.sum(axis=1)
+
+print(snv_df)
+'''
+# print(bl_df)
+# print(dba_df)
+# print(bl_dba_ratios)
--- src/visualize.py
+++ src/visualize.py
@@ -2,55 +2,79 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+params = {'legend.fontsize': 'x-small',
+          'legend.framealpha': 0.5,
+          'legend.title_fontsize': 'small'}
+plt.rcParams.update(params)
+
 
 def mutation_spectrum_barcharts(muts_strains, show=True, save=False, results_dir=None):
-    snv_tots = muts_strains.sum(axis=0, level=0).sum(axis=1)
+    sum_lvl = [0, 1, 2]
+
+    snv_tots = muts_strains.sum(axis=0, level=sum_lvl).sum(axis=1)
     tot = snv_tots.sum()
     snv_frac = snv_tots / tot
 
-    bl_snv_tots = muts_strains.xs('BL', level='ht').sum(axis=0, level=0).sum(axis=1)
+    bl_snv_tots = muts_strains.xs('BL', level='ht').sum(axis=0, level=sum_lvl).sum(axis=1)
     bl_tot = bl_snv_tots.sum()
     bl_snv_frac = bl_snv_tots / bl_tot
-    dba_snv_tots = muts_strains.xs('DBA', level='ht').sum(axis=0, level=0).sum(axis=1)
+    dba_snv_tots = muts_strains.xs('DBA', level='ht').sum(axis=0, level=sum_lvl).sum(axis=1)
     dba_tot = dba_snv_tots.sum()
     dba_snv_frac = dba_snv_tots / dba_tot
 
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
-
-    ax1.bar(snv_tots.index, snv_tots)
-
-    ax2.bar(bl_snv_tots.index, bl_snv_tots, align='edge', width=0.35, label='C57BL/6J')
-    ax2.bar(dba_snv_tots.index, dba_snv_tots, align='edge', width=-0.35, label='DBA/2J')
-
-    ax3.bar(snv_frac.index, snv_frac)
-
-    ax4.bar(bl_snv_frac.index, bl_snv_frac, align='edge', width=0.35, label='C57BL/6J')
-    ax4.bar(dba_snv_frac.index, dba_snv_frac, align='edge', width=-0.35, label='DBA/2J')
-
-    ax1.set_ylabel('Counts')
-    ax1.set_xlabel('Mutation Type')
-    ax1.set_title('Mutation Spectrum Counts')
-
-    ax2.set_ylabel('Counts')
-    ax2.set_xlabel('Mutation Type')
-    ax2.set_title('Counts by Haplotype')
-    ax2.legend()
-
-    ax3.set_ylabel('Fraction')
-    ax3.set_xlabel('Mutation Type')
-    ax3.set_title('Mutation Spectrum Fractions')
-
-    ax4.set_ylabel('Fraction')
-    ax4.set_xlabel('Mutation Type')
-    ax4.set_title('Fractions by Haplotype')
-    ax4.legend()
-
-    fig.tight_layout(pad=2.0)
-
-    if save:
-        plt.savefig(results_dir + 'mutation_spectrum_distr.pdf')
     if show:
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
+        temp_bool = True
+        if not temp_bool:
+            ax1.bar(snv_tots.index, snv_tots)
+
+            ax2.bar(bl_snv_tots.index, bl_snv_tots, align='edge', width=0.35, label='C57BL/6J')
+            ax2.bar(dba_snv_tots.index, dba_snv_tots, align='edge', width=-0.35, label='DBA/2J')
+
+            ax3.bar(snv_frac.index, snv_frac)
+
+            ax4.bar(bl_snv_frac.index, bl_snv_frac, align='edge', width=0.35, label='C57BL/6J')
+            ax4.bar(dba_snv_frac.index, dba_snv_frac, align='edge', width=-0.35, label='DBA/2J')
+        else:
+            snv_tots.plot(kind='bar', ax=ax1)
+
+            temp = muts_strains.sum(axis=1)
+            temp.unstack([3]).plot(kind='bar', ax=ax2, stacked=True)
+            # bl_snv_tots.unstack(0).plot(kind='bar', ax=ax2, position=0)
+            # dba_snv_tots.unstack(0).plot(kind='bar', ax=ax2, position=1)
+
+            snv_frac.unstack(0).plot(kind='bar', ax=ax3)
+
+        ax1.set_ylabel('Counts')
+        ax1.set_xlabel('Mutation Type')
+        ax1.set_title('Mutation Spectrum Counts')
+
+        ax2.set_ylabel('Counts')
+        ax2.set_xlabel('Mutation Type')
+        ax2.set_title('Counts by Haplotype')
+        ax2.legend()
+
+        ax3.set_ylabel('Fraction')
+        ax3.set_xlabel('Mutation Type')
+        ax3.set_title('Mutation Spectrum Fractions')
+
+        ax4.set_ylabel('Fraction')
+        ax4.set_xlabel('Mutation Type')
+        ax4.set_title('Fractions by Haplotype')
+        ax4.legend()
+
+        fig.tight_layout(pad=2.0)
+
+        if save:
+            plt.savefig(results_dir + 'mutation_spectrum_distr.pdf')
+
         plt.show()
+
+    snv_df = pd.concat([snv_tots, snv_frac], axis=1, keys=['counts', 'fracs'])
+    bl_df = pd.concat([bl_snv_tots, bl_snv_frac], axis=1, keys=['counts', 'fracs'])
+    dba_df = pd.concat([dba_snv_tots, dba_snv_frac], axis=1, keys=['counts', 'fracs'])
+
+    return snv_df, bl_df, dba_df
 
 '''
 def visualize_strain_distrb(df):
@@ -75,9 +99,9 @@ def visualize_strain_distrb(df):
     print(strain_counts)
 
 
+'''
 
-
-
+'''
 def mutation_spectrum_heatmap(df):
     b_df = df[df['haplotype'] == 0]
     d_df = df[df['haplotype'] == 1]
@@ -99,8 +123,4 @@ def mutation_spectrum_heatmap(df):
     plt.show()
 
     return ratio_props
-
-
-
 '''
-
